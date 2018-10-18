41fe83d076283ef074319d883e8048c66160d776
jason <342690199@qq.com>
2018-10-18 12:12:52 +0800

    update

--- miniapp/pages/discover/discover.js
+++ miniapp/pages/discover/discover.js
@@ -5,28 +5,89 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    circular: true,
+    interval: 5000,
+    duration: 1000
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.mapCtx = wx.createMapContext('myMap')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    // 获取定位
+    wx.getLocation({
+      type: 'gcj02',
+      success: res=>{
+        console.log('res...', res);
+      }
+    })
+
+    // 获取canvas实例
+    let canvas = wx.createCanvasContext("canvas");
+    this.canvas = canvas;
+    // 绘制吴彦祖
+
+    canvas.drawImage('../../assets/timg.png', 0,0,750,975,0,0,375,487);
+    canvas.draw();
+    canvas.save();
+  },
+  selectImg(){
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res)=>{
+        // tempFilePath可以作为img标签的src属性显示图片
+        console.log('选择了一张图片', res.tempFilePaths);
+        const tempFilePaths = res.tempFilePaths
+        this.canvas.drawImage(res.tempFilePaths[0], 100,100,100,120);
+        this.canvas.draw(true);
+      }
+    })
+  },
+  saveImg(){
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 750,
+      height: 975,
+      destWidth: 750,
+      destHeight: 975,
+      canvasId: 'canvas',
+      success(res) {
+        console.log(res.tempFilePath)
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success(res) {
+              console.log('res...', res);
+          },
+          fail(err) {
+              console.log('err...', err);
+          }
+        })
+      }
+  })
   },
 
   /**
@@ -41,6 +102,43 @@ Page({
    */
   onUnload: function () {
   
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation({
+      latitude:40.0396220000,
+      longitude:116.2994380000,
+    })
+  },
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 0,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude:40.0396220000,
+        longitude:116.2994380000,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude:40.0396220000,
+        longitude:116.2994380000,
+      }]
+    })
   },
 
   /**
