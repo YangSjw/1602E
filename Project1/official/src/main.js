395bc16aeba7b1ca945993fabca3e734194d7ab0
jason <342690199@qq.com>
2018-10-24 19:47:57 +0800

    update

--- Project1/official/src/main.js
+++ Project1/official/src/main.js
@@ -1,13 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-// 引入路由
-import router from './router';
-// 引入vuex
-import store from './store';
+// import Vue from 'vue'
+// import App from './App.vue'
+// // 引入路由
+// import router from './router';
+// // 引入vuex
+// import store from './store';
 
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   render: h => h(App)
+// })
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import kobe from './assets/kobe.jpg';
+import black from './assets/black.jpg';
+const Index = ()=>{
+    return <React.Fragment>
+        <h1>我是React</h1>
+        <img src={kobe}/>
+        <img src={black}/>
+    </React.Fragment>
+}
+ReactDOM.render(<Index />, document.querySelector('#app'));
